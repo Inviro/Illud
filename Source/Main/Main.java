6fd893816d1e69c2925bda3e9eefbe671c235a8f
MandoGuardado <65988647+MandoGuardado@users.noreply.github.com>
2020-06-03 23:12:41 -0700

    Removed additional import libraries
    Revmoved additional lines

--- Source/Main/Main.java
+++ Source/Main/Main.java
@@ -1,7 +1,6 @@
 package Source.Main;
-import Libraries.MaryTTS.Tutorial.TextToSpeech;
 import Source.GUI.Window;
-import marytts.modules.synthesis.Voice; // Used if you want to print out the available voices
+
 
 public class Main {
     public static void main (String[] args){
