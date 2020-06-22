64fb41e7e8c4c3e4728793a068873d24488a5ab9
Inviro <prelusiondilemma@gmail.com>
2020-06-21 23:45:49 -0700

    Implemented Armando's Find and Replace

--- Source/GUI/FindandReplace.java
+++ Source/GUI/FindandReplace.java
@@ -1,24 +1,25 @@
 package Source.GUI;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class FindandReplace extends JDialog{
     private JPanel findAndReplace;
     private JButton buttonFind;
     private JTextField t1;
     private JTextField t2;
-    private JTextArea area = new JTextArea();
+    private JTextArea area;
 
-    public  FindandReplace() {
+    public FindandReplace(JTextArea area) {
+        this.area = area;
         setContentPane(findAndReplace);
         setModal(true);
         getRootPane().setDefaultButton(buttonFind);
-
         buttonFind.addActionListener(e -> onFind());
-
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        this.setSize(600, 150);                     // Setting Dialog Size
+        this.setLocationRelativeTo(null);                       // Centers Dialog
+        this.setTitle("Find and Replace");                      // Sets Dialog Title
     }
 
     private void onFind() {
@@ -31,16 +32,13 @@ public class FindandReplace extends JDialog{
 
         String c = area.getText(); // Assign text from mainTextArea
 
-        JOptionPane.showMessageDialog(this,"Are you sure you want to " +
-                "replace \"" + a + "\" with \"" + b + "\" " +
-                "? \nPlease click ok." );
+        JOptionPane.showMessageDialog(this,
+                "Are you sure you want to " +
+                        "replace \"" + a + "\" with \"" + b + "\" " +
+                        "? \nPlease click ok.");
 
-        String d = c.replaceAll(a2,b2); // Replace a2 with b2
+        String d = c.replaceAll(a2, b2); // Replace a2 with b2
         area.setText(d); // Place the corrected text into the main panel
-        dispose();
-    }
-
-    public void setFindAndReplace(JTextArea copy) {
-        area = copy; // Set JtextArea to mainTextArea
+        this.dispose();
     }
 }
