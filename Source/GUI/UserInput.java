959818e716049c7c096be441dc6f922e0b435c3b
Inviro <prelusiondilemma@gmail.com>
2020-07-02 18:10:18 -0700

    Removed unnamed GUI components from source code
    Removed the name of panels that were used for organization so that Intellij formbuilder would add them in without them cluttering up the code

--- Source/GUI/UserInput.java
+++ Source/GUI/UserInput.java
@@ -4,17 +4,12 @@ package Source.GUI;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JList;
-import javax.swing.JScrollPane;
-import javax.swing.JButton;
 
 public class UserInput {
     // Class Variables
     private JPanel MainPanel;
     private JTextArea mainTextArea;
     private JList<String> jList;
-    private JPanel jPanel;
-    private JScrollPane scrollPane;
-    private JButton findAndReplace; // Button added to main GUI
 
     // Getters
     public JPanel getMainPanel() { return MainPanel; }
